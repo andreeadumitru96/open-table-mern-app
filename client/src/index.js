3111051dc270a3e8d63c52b36b998021345543fc
Andreea <dumitru_andreea96@yahoo.com>
2018-03-21 12:21:29 +0100

    NotFoundRoute added

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppContainer from './components/shared/AppContainer/AppContainer'
+import LoginContainer from './components/shared/LoginContainer/LoginContainer';
+import NotFoundRoute from './components/shared/NotFoundRoute/NotFoundRoute';
+
 import registerServiceWorker from './registerServiceWorker';
-import MuiThemeProvieder from 'material-ui/styles/MuiThemeProvider';
-ReactDOM.render(<MuiThemeProvieder><App /></MuiThemeProvieder>, document.getElementById('root'));
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+ReactDOM.render(<MuiThemeProvider>
+    <Router>
+        <Switch>
+            <Route exact path='/' component={AppContainer} />
+            <Route exact path='/login' component={LoginContainer} />
+            <Route component={NotFoundRoute} />
+        </Switch>
+    </Router>
+</MuiThemeProvider>, 
+document.getElementById('root'));
 
 registerServiceWorker();
