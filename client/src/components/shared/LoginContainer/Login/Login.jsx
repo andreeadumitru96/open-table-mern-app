bbc1614244b0e86960c08c71b49562b89741b612
Andreea <dumitru_andreea96@yahoo.com>
2018-03-22 11:14:55 +0100

    refreshing login component

--- client/src/components/shared/LoginContainer/Login/Login.jsx
+++ client/src/components/shared/LoginContainer/Login/Login.jsx
@@ -3,6 +3,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
 import './Login.css';
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -14,17 +15,33 @@ class Login extends Component {
     return (
       <div className="login">
         <form className="login__form">
-            <div className="login__form-email">
-                <TextField  floatingLabelText="Email"/>  
-            </div>
+          <div className="login__p">
+            <p>LOG IN</p>
+          </div>
+          <div className="login__form-email">
+              <TextField  
+                inputStyle={{color: 'white'}}
+                floatingLabelStyle={{color: 'white'}}
+                floatingLabelText="Email"
+              />  
+          </div>
 
-            <div className="login__from-password">
-                <TextField  floatingLabelText="Password"/>
-            </div>
+          <div className="login__from-password">
+              <TextField
+                inputStyle={{color: 'white'}}
+                floatingLabelStyle={{color: 'white'}}
+                floatingLabelText="Password"
+                // type="password"
+              />
+          </div>
 
-            <div className="login__form-sign-in-button">
-                <RaisedButton label="LOG IN"/>
-            </div>
+          <div className="login__form-sign-in-button">
+              <RaisedButton label="LOG IN"/>
+          </div>
+
+          <div className="login__form-forgot-password" >
+            <a href="" className="login__form-forgot-password-anchor">Forgot your password?</a>
+          </div>
             
                         
         </form>
