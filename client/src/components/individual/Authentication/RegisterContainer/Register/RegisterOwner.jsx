dbfe0d2fc2e7aeca38349abc51bfb969f03cc31c
Andreea <dumitru_andreea96@yahoo.com>
2018-03-23 10:53:45 +0100

    Register Owner View added

---
+++ client/src/components/individual/Authentication/RegisterContainer/Register/RegisterOwner.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+import './Register.css';
+
+class RegisterOwner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+        <div className="register">
+            <form className="register__form">
+                <div className="register__form-p">
+                    <p>Discover your customers</p>
+                </div>
+
+                <div className="register__form-location-name">
+                    <TextField
+                        inputStyle={{color: 'white'}}
+                        floatingLabelStyle={{color: 'white'}}
+                        floatingLabelText="Location Name"
+                    />  
+                </div>
+
+                <div className="register__form-address">
+                    <TextField
+                        inputStyle={{color: 'white'}}
+                        floatingLabelStyle={{color: 'white'}}
+                        floatingLabelText="Address"
+                    />
+                </div>
+
+                <div className="register__form-phone">
+                    <TextField
+                        inputStyle={{color: 'white'}}
+                        floatingLabelStyle={{color: 'white'}}
+                        floatingLabelText="Phone"
+                    />
+                </div>
+
+                <div className="register__form-email">
+                    <TextField
+                        inputStyle={{color: 'white'}}
+                        floatingLabelStyle={{color: 'white'}}
+                        floatingLabelText="Email"
+                    />
+                </div>
+                
+                <div className="register__form-password">
+                    <TextField
+                        inputStyle={{color: 'white'}}
+                        floatingLabelStyle={{color: 'white'}}
+                        floatingLabelText="Password"
+                    />
+                </div>
+
+                <div className="register__form-repeat-password">
+                    <TextField
+                        inputStyle={{color: 'white'}}
+                        floatingLabelStyle={{color: 'white'}}
+                        floatingLabelText="Repeat Password"
+                    />
+                </div>
+
+                <div className="register__form-sign-up-button">
+                    <RaisedButton label="SIGN UP"/>
+                </div>
+
+                <div className="register__form-forgot-password">
+                    <a href="" className="login__form-forgot-password-anchor"> I've already have an account </a>
+                </div>
+            </form>
+        </div>
+    );
+  }
+}
+
+export default RegisterOwner;
