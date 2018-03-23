f4abc776d674aa187a20b96f6f0259e895a9714c
Andreea <dumitru_andreea96@yahoo.com>
2018-03-23 12:17:18 +0100

    Moved Routes to App Component

--- client/src/components/individual/AppContainer/AppContainer.jsx
+++ client/src/components/individual/AppContainer/AppContainer.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
 
-import App from './App/App'
+import App from './App/App';
+import LoginContainer from '../Authentication/LoginContainer/LoginContainer';
+import RegisterContainer from '../Authentication/RegisterContainer/RegisterContainer';
+import NotFoundRoute from '../../shared/NotFoundRoute/NotFoundRoute';
 
 class AppContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+}
 
   render() {
     return (
-        <App/>
+        <App>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={AppContainer} />
+                    <Route exact path='/login' component={LoginContainer}/>
+                    <Route exact path='/register' render={() => <RegisterContainer isCustomer={'true'} />}/>
+                    <Route component={NotFoundRoute} />
+                </Switch>
+            </Router>
+        </App>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppContainer from './components/individual/AppContainer/AppContainer';
-import LoginContainer from './components/individual/Authentication/LoginContainer/LoginContainer';
-import RegisterContainer from './components/individual/Authentication/RegisterContainer/RegisterContainer';
-import NotFoundRoute from './components/shared/NotFoundRoute/NotFoundRoute';
-
-
 import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom';
-
-ReactDOM.render(<MuiThemeProvider>
-    <Router>
-        <Switch>
-            <Route exact path='/' component={AppContainer} />
-            <Route exact path='/login' component={LoginContainer}/>
-            <Route exact path='/register' render={() => <RegisterContainer isCustomer={'true'} />}/>
-            <Route component={NotFoundRoute} />
-        </Switch>
-    </Router>
-</MuiThemeProvider>, 
-document.getElementById('root'));
+ReactDOM.render(<MuiThemeProvider/>, document.getElementById('root'));
 
 registerServiceWorker();
